5a83efa95f2c7817ae43fe0b4932a9d223266bdf
aur3l14no <muzhi.yu1@gmail.com>
2023-12-06 00:06:35 +0800

    Update cache process
    Other packages are included in inputs instead of in scripts

--- flake.nix
+++ flake.nix
@@ -4,21 +4,26 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils";
+    yaml2ninx.url = "github:euank/yaml2nix";
+    sops-nix.url = "github:Mic92/sops-nix";
   };
 
   outputs = {
     self,
     nixpkgs,
     flake-utils,
+    ...
   } @ inputs:
     flake-utils.lib.eachSystem ["x86_64-linux" "aarch64-linux" "aarch64-darwin"] (
       system: let
         pkgs = import nixpkgs {
           inherit system;
         };
-      in rec {
-        packages = (import ./pkgs/sing-box) pkgs;
-        checks = packages;
+      in {
+        packages =
+          (import ./pkgs/sing-box) pkgs
+          // inputs.yaml2ninx.outputs.packages.${system}.yaml2nin
+          // inputs.sops-nix.outputs.packages.${system}.sops-install-secrets;
         devShell = pkgs.mkShellNoCC {
           packages = with pkgs; [
             act
--- justfile
+++ justfile
@@ -9,14 +9,10 @@ cache:
         jq -r '.[].outputs | to_entries[].value' | cachix push aur3l14no
     end
 
+    # might fail, do best we can
     for system in x86_64-linux aarch64-linux aarch64-darwin
         nix flake show --json \
         | jq  ".packages.\"$system\"|keys[]" \
         | xargs -I {} nix build .#packages.$system.'"{}"' --json \
         | push
-
-        nix build github:euank/yaml2nix#packages.$system.default --json | push
-        nix build github:Mic92/sops-nix#packages.$system.default --json | push
-        nix build github:Mic92/sops-nix#packages.$system.sops-install-secrets --json | push
     end
-
