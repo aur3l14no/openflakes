f1a0f1b5ecfa9593ff3cdb8f37dd2545ef2d35a6
aur3l14no <muzhi.yu1@gmail.com>
2024-12-03 19:52:36 +0800

    Simplify flake inputs

--- flake.nix
+++ flake.nix
@@ -2,52 +2,54 @@
   description = "My open flakes";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    nixpkgs-unstable-nixos.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
-    yaml2nix.url = "github:euank/yaml2nix";
-    sops-nix = {
-      url = "github:Mic92/sops-nix";
-      inputs.nixpkgs.follows = "nixpkgs-unstable-nixos";
-    };
   };
 
   nixConfig = {
     extra-substituters = [ "https://aur3l14no.cachix.org" ];
-    extra-trusted-public-keys = [ "aur3l14no.cachix.org-1:jxuBM4n3aEvFMkxO1I/LqmAIExoXIkNzAj6tZAd6oC4=" ];
+    extra-trusted-public-keys = [
+      "aur3l14no.cachix.org-1:jxuBM4n3aEvFMkxO1I/LqmAIExoXIkNzAj6tZAd6oC4="
+    ];
   };
 
   outputs =
-    { self
-    , nixpkgs
-    , flake-utils
-    , ...
-    } @ inputs:
-    flake-utils.lib.eachSystem [ "x86_64-linux" "aarch64-linux" "aarch64-darwin" ] (
-      system:
-      let
-        pkgs = nixpkgs.legacyPackages.${system};
-        lib = pkgs.lib;
-      in
-      {
-        packages =
-          (import ./pkgs/sing-box) pkgs
-          // {
-            yaml2nix = inputs.yaml2nix.outputs.packages.${system}.yaml2nix;
-            inherit (inputs.sops-nix.outputs.packages.${system})
-              sops-install-secrets
-              ;
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+      ...
+    }@inputs:
+    flake-utils.lib.eachSystem
+      [
+        "x86_64-linux"
+        "aarch64-linux"
+        "aarch64-darwin"
+      ]
+      (
+        system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+        in
+        {
+          packages = (import ./pkgs/sing-box) pkgs // { };
+          formatter = pkgs.alejandra;
+          devShell = pkgs.mkShellNoCC {
+            packages = with pkgs; [
+              act
+              fish
+              just
+              jq
+              (python3.withPackages (
+                ps: with ps; [
+                  requests
+                  requests-cache
+                  tenacity
+                  typer
+                ]
+              ))
+            ];
           };
-        formatter = pkgs.alejandra;
-        devShell = pkgs.mkShellNoCC {
-          packages = with pkgs; [
-            act
-            fish
-            just
-            jq
-            (python311.withPackages (ps: with ps; [ requests requests-cache tenacity typer ]))
-          ];
-        };
-      }
-    );
+        }
+      );
 }
