141084c416c9a507cb5893baebaf8cf0be21821f
aur3l14no <muzhi.yu1@gmail.com>
2024-12-03 21:35:35 +0800

    Fix typo

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "My open flakes";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
