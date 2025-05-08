5dfee617810c6aac92b252818680918972ac1fba
aur3l14no <muzhi.yu1@gmail.com>
2025-05-08 10:48:22 +0800

    New sing-box versions: 1.12.0-beta.11, 1.12.0-beta.10, 1.12.0-beta.9, 1.12.0-beta.8, 1.12.0-beta.7

--- pkgs/sing-box/1.12.0-beta.10/default.nix
+++ pkgs/sing-box/1.12.0-beta.10/default.nix
@@ -1,16 +1,88 @@
 {
-  sing-box,
+  lib,
+  stdenv,
   buildGoModule,
   fetchFromGitHub,
+  installShellFiles,
+  buildPackages,
+  coreutils,
+  nix-update-script,
+  nixosTests,
 }:
-sing-box.overrideAttrs (oldAttrs: rec {
+
+buildGoModule rec {
+  pname = "sing-box";
   version = "1.12.0-beta.10";
+
   src = fetchFromGitHub {
     owner = "SagerNet";
-    repo = "sing-box";
+    repo = pname;
     rev = "v${version}";
     hash = "sha256-Uj7HX+mYzvPXIsEGnNoGccwu7xpeINZFUPNxqQFS4ow=";
   };
+
   vendorHash = "sha256-ewOQwny7EpjWRMZLp62sosvA1LFeLhFGk1iLfdX8+uU=";
-  ldflags = [ "-X=github.com/sagernet/sing-box/constant.Version=${version}" ];
-})
+
+  tags =
+    [
+      "with_quic"
+      "with_dhcp"
+      "with_wireguard"
+      "with_utls"
+      "with_acme"
+      "with_clash_api"
+      "with_gvisor"
+    ]
+    ++ (
+      if lib.versionAtLeast version "1.12" then
+        # >= 1.12
+        [
+          "with_tailscale"
+        ]
+      else
+        # <= 1.11
+        [
+          "with_ech"
+          "with_reality_server"
+        ]
+    );
+
+  subPackages = [
+    "cmd/sing-box"
+  ];
+
+  nativeBuildInputs = [ installShellFiles ];
+
+  ldflags = [
+    "-X=github.com/sagernet/sing-box/constant.Version=${version}"
+  ];
+
+  postInstall =
+    let
+      emulator = stdenv.hostPlatform.emulator buildPackages;
+    in
+    ''
+      installShellCompletion --cmd sing-box \
+        --bash <(${emulator} $out/bin/sing-box completion bash) \
+        --fish <(${emulator} $out/bin/sing-box completion fish) \
+        --zsh  <(${emulator} $out/bin/sing-box completion zsh )
+
+      substituteInPlace release/config/sing-box{,@}.service \
+        --replace-fail "/usr/bin/sing-box" "$out/bin/sing-box" \
+        --replace-fail "/bin/kill" "${coreutils}/bin/kill"
+      install -Dm444 -t "$out/lib/systemd/system/" release/config/sing-box{,@}.service
+    '';
+
+  passthru = {
+    updateScript = nix-update-script { };
+    tests = { inherit (nixosTests) sing-box; };
+  };
+
+  meta = with lib; {
+    homepage = "https://sing-box.sagernet.org";
+    description = "Universal proxy platform";
+    license = licenses.gpl3Plus;
+    maintainers = with maintainers; [ nickcao ];
+    mainProgram = "sing-box";
+  };
+}
